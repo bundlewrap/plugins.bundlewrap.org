4688a807e2673ee1cf82d6d06404c427cee3aced
Torsten Rehn <torsten@rehn.email>
2014-05-24 14:08:50 +0200

    cache index data for 3 minutes

--- index.py
+++ index.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 from requests import get
+from werkzeug.contrib.cache import SimpleCache
 
 INDEX_URL = "https://raw.githubusercontent.com/blockwart/plugins/master/index.json"
+CACHE_TIMEOUT = 3 * 60  # seconds
 
 app = Flask(__name__)
+cache = SimpleCache()
 
 
 @app.route('/')
 def index():
-    index_data = get(INDEX_URL)
+    index_data = cache.get('index')
+    if index_data is None:
+        print("GET")
+        index_data = get(INDEX_URL).json()
+        cache.set('index', index_data, timeout=CACHE_TIMEOUT)
     return render_template(
         "index.html",
-        index=sorted(index_data.json().items()),
+        index=sorted(index_data.items()),
     )
