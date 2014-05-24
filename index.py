5503608d65b3800ea8c1e1faef9f9794cf4e2296
Torsten Rehn <torsten@rehn.email>
2014-05-24 11:26:07 +0200

    proxy JSON index

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from requests import get
+
+INDEX_URL = "https://raw.githubusercontent.com/blockwart/plugins/master/index.json"
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
-
-
+def index():
+    index_data = get(INDEX_URL)
+    return index_data.text
