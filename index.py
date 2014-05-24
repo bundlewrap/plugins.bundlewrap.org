36a88530c45980d51b9f5969fddd1b3b81c7a9de
Torsten Rehn <torsten@rehn.email>
2014-05-24 11:37:14 +0200

    render to HTML

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from requests import get
 
 INDEX_URL = "https://raw.githubusercontent.com/blockwart/plugins/master/index.json"
@@ -9,4 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     index_data = get(INDEX_URL)
-    return index_data.text
+    return render_template(
+        "index.html",
+        index=sorted(index_data.json().items()),
+    )
